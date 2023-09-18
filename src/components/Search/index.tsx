d34422ed265557867ce75090b87a4bc2f9aa8b95
Dima <dimaberry1@gmail.com>
2023-09-18 20:24:39 +0200

    feat: init tailwind and add Search component

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Search: React.FC = () => {
+  return (
+    <div className="">
+      <input className="" placeholder="Search..." />
+      <button className="">Click</button>
+    </div>
+  );
+};
+
+export default Search;
