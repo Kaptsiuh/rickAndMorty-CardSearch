06398c510f6d2bf362bde9b67af52eda125fcc20
Dima <dimaberry1@gmail.com>
2023-09-22 23:32:42 +0200

    fix: fix axios

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,65 +1,29 @@
-// import axios from 'axios';
+import axios from 'axios';
 import React from 'react';
-// import { useNavigate, useParams } from 'react-router-dom';
 
-const defaultEndpoint = 'https://rickandmortyapi.com/api/character/';
+const src = 'https://rickandmortyapi.com/api/character/';
 
-export async function getServerSideProps() {
-  const res = await fetch(defaultEndpoint);
-  const data = await res.json();
-  return {
-    props: {
-      data,
-    },
-  };
-}
+const Card: React.FC = () => {
+  const [rickAndMortyCards, setRickAndMortyCards] = React.useState([]);
 
-const Card: React.FC = ({ data }) => {
-  //   const [card, setCard] = React.useState<{
-  //     imageUrl: string;
-  //     title: string;
-  //     price: number;
-  //   }>();
-  //   // const { id } = useParams();
-  //   const navigate = useNavigate();
-
-  //   React.useEffect(() => {
-  //     async function fetchCard() {
-  //       try {
-  //         const { data } = await axios.get(`https://rickandmortyapi.com/api/character/`);
-  //         setCard(data);
-  //       } catch (err) {
-  //         navigate('/');
-  //       }
-  //     }
-
-  //     fetchCard();
-  //   }, []);
-
-  //   if (!card) {
-  //     return <>Loading...</>;
-  //   }
-
-  //   console.log(card);
-
-  const { results } = data;
+  React.useEffect(() => {
+    axios.get(src).then((data) => {
+      setRickAndMortyCards(data.data.results);
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        {results.map((result) => {
-          const { id, name, image } = result;
-
-          return (
-            <li key={id}>
-              <a href="">
-                <img src={image} alt={`${name} Thumb`} />
-                <h3>{name}</h3>
-              </a>
-            </li>
-          );
-        })}
-      </div>
+      {rickAndMortyCards.map((person) => {
+        return (
+          <li key={person.id}>
+            <a href={person.url}>
+              <img src={person.image} alt={`${person.name} Thumb`} />
+              <h3>{person.name}</h3>
+            </a>
+          </li>
+        );
+      })}
     </>
   );
 };
