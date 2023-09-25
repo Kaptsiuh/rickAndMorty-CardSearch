186fca8cfb205209ce77a9ebf7f183f343f60a69
Dima <dimaberry1@gmail.com>
2023-09-25 15:40:43 +0200

    feat: init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import MainLayout from './layouts/MainLayouts';
-import Home from './pages/Home';
-
+// import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route path="" element={<Home />} />
-      </Route>
-    </Routes>
-  );
-}
+const App: React.FC = () => {
+  return <></>;
+};
 
 export default App;
