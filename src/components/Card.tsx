a4ec094bb15ea1ddcbfd355627276d749132b8aa
Dima <dimaberry1@gmail.com>
2023-09-26 19:55:51 +0200

    feat: create components Card use class components

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,29 +3,107 @@ import React from 'react';
 
 const src = 'https://rickandmortyapi.com/api/character/';
 
-const Card: React.FC = () => {
-  const [rickAndMortyCards, setRickAndMortyCards] = React.useState([]);
+interface RequestOptions {
+  created: string;
+  episode: string[];
+  gender: string;
+  id: number;
+  image: string;
+  location: {
+    name: string;
+    url: string;
+  };
+  name: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  species: string;
+  status: string;
+  type: string;
+  url: string;
+} // перепроверить правильность, засунул в props конструктора и в map
 
-  React.useEffect(() => {
-    axios.get(src).then((data) => {
-      setRickAndMortyCards(data.data.results);
-    });
-  }, []);
+class Card extends React.Component {
+  constructor(props: RequestOptions) {
+    super(props);
+    this.state = {
+      cards: [],
+      loading: true,
+      error: null,
+    };
+  }
 
-  return (
-    <>
-      {rickAndMortyCards.map((person) => {
-        return (
+  componentDidMount(): void {
+    axios
+      .get(src)
+      .then((data) => {
+        this.setState({
+          cards: data.data.results,
+          loading: false,
+          error: null,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          cards: [],
+          loading: false,
+          error: error,
+        });
+      });
+  }
+
+  render() {
+    const { cards, loading, error } = this.state;
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    }
+
+    return (
+      <div>
+        {cards.map((person: RequestOptions) => (
           <div key={person.id}>
             <a href={person.url}>
               <img src={person.image} alt={`${person.name} Thumb`} />
               <h3>{person.name}</h3>
             </a>
           </div>
-        );
-      })}
-    </>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+}
+
+// const src = 'https://rickandmortyapi.com/api/character/';
+
+// const Card: React.FC = () => {
+//   const [rickAndMortyCards, setRickAndMortyCards] = React.useState([]);
+
+//   React.useEffect(() => {
+//     axios.get(src).then((data) => {
+//       setRickAndMortyCards(data.data.results);
+//     });
+//   }, []);
+
+//   return (
+//     <>
+//       {rickAndMortyCards.map((person) => {
+//         return (
+//           <div key={person.id}>
+//             <a href={person.url}>
+//               <img src={person.image} alt={`${person.name} Thumb`} />
+//               <h3>{person.name}</h3>
+//             </a>
+//           </div>
+//         );
+//       })}
+//     </>
+//   );
+// };
 
 export default Card;
